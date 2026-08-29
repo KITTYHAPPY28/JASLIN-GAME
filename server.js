d0760d35ddc3bff76f339f08de92bd83119cf81e
KITTYHAPPY28 <anjasanjas32836@gmail.com>
2026-08-29 19:45:32 +0700

    server.js

--- server.js
+++ server.js
@@ -105,6 +105,8 @@ const JASLIN_DECIMALS = 9;
 const MIN_WITHDRAW_JASLIN =
   2000;
 
+const MAX_LEVEL = 5000;
+const LEVEL_PRICE_USD = 1;
 
 /* =========================================
    SUPABASE
